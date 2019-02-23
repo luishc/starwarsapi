1f9e584ac39bda8343e858f5e7df8d7b0e1385e6
Luis Henrique Carvalho do Nascimento <lhcnascimento@gmail.com>
2019-02-23 16:45:10 -0300

    Unit test to get number of appearances in films when save planet

---
+++ __tests__/unit/planeta.test.js
@@ -0,0 +1,27 @@
+const Planeta = require('../../src/app/models/Planeta');
+
+describe('Planet', () =>{
+    it('should get number of appearances in films of exinting planet', async () =>{
+        let planeta = await Planeta.create({
+            nome: 'Alderaan',
+            clima: 'temperate',
+            terreno: 'grasslands, mountains'
+        });
+
+        expect(Object.keys(planeta.toObject())).toEqual(
+            expect.arrayContaining(['_id', 'aparicoes_filmes'])
+        );
+        expect(planeta.aparicoes_filmes).toBeGreaterThanOrEqual(0);
+    });
+
+    it('should not contains number of appearances in films of planet that doesnt extist', async () =>{
+        let planeta = await Planeta.create({
+            nome: 'Terra',
+            clima: 'temperate',
+            terreno: 'grasslands, mountains'
+        });
+
+        expect(planeta.toObject()).not.toHaveProperty('aparicoes_filmes');
+        
+    });
+});
---
+++ src/app/models/Planeta.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const swapi = require('swapi-node');
+
+var planetaSchema = new Schema({
+    nome: {
+        type: String
+    },
+    clima: {
+        type: String
+    },
+    terreno: {
+        type: String
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    aparicoes_filmes:{
+        type: Number
+    }
+});
+
+planetaSchema.pre('save', async function(next) {
+    var result = await swapi.get('https://swapi.co/api/planets?search=' + this.nome);
+    
+    if(result.count > 0){
+        this.aparicoes_filmes = result.results[0].films.length;
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Planeta', planetaSchema);
